66fdc192452e813831eeced32bc50cd955a769f4
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-10-21 15:11:01 -0700

    Final commit for the Data website. (I worked on this on another repository on accident so the previous commits don't appear on this repo.)

---
+++ script.js
@@ -0,0 +1,76 @@
+
+function run1(){
+    if(document.getElementById("image1").src == "https://upload.wikimedia.org/wikipedia/en/4/4a/Dr_John_Zoidberg.png") {
+        document.getElementById("image1").src = "http://multsonline.ru/uploads/posts/2012-03/1333118914_a_37204f0.jpg"
+    }
+    else {
+        document.getElementById("image1").src = "https://upload.wikimedia.org/wikipedia/en/4/4a/Dr_John_Zoidberg.png"
+    }
+}
+
+
+function run2(){
+    if(document.getElementById("image2").src == "http://cdn.instructables.com/FVZ/OO6C/H7ANSLLA/FVZOO6CH7ANSLLA.MEDIUM.jpg") {
+        document.getElementById("image2").src = "http://i.kinja-img.com/gawker-media/image/upload/fhlaudxmayhkalspss09.jpg"
+    }
+    else {
+        document.getElementById("image2").src = "http://cdn.instructables.com/FVZ/OO6C/H7ANSLLA/FVZOO6CH7ANSLLA.MEDIUM.jpg"
+    }
+}
+
+function run3(){
+    if(document.getElementById("image3").src == "http://cs4.pikabu.ru/images/previews_comm/2014-03_2/13943870278693.gif") {
+        document.getElementById("image3").src = "http://i700.photobucket.com/albums/ww10/Kenny_Kamakazi/Zoidberg-Why.gif"
+    }
+    else {
+        document.getElementById("image3").src = "http://cs4.pikabu.ru/images/previews_comm/2014-03_2/13943870278693.gif"
+    }
+}
+function run4(){
+    if(document.getElementById("image4").src == "http://vignette3.wikia.nocookie.net/uncyclopedia/images/1/17/Zoidberg.png/revision/latest?cb=20100527123605") {
+        document.getElementById("image4").src = "http://forums.imore.com/attachments/ask-question/41574d1383538518-siri-unable-tell-me-when-sun-will-rise-tomorrow-zoidberg-hooray-494x394.jpeg"
+    }
+    else {
+        document.getElementById("image4").src = "http://vignette3.wikia.nocookie.net/uncyclopedia/images/1/17/Zoidberg.png/revision/latest?cb=20100527123605"
+    }
+}
+function run5(){
+    if(document.getElementById("image5").src == "http://img05.deviantart.net/5c20/i/2007/210/3/6/zoidberg_vector_by_enohp.jpg") {
+        document.getElementById("image5").src = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS7Im5NNuvit4W-dsjQIZ1LJrf5ZKsCpKl6vXY0qiTcnNvFuBZP"
+    }
+    else {
+        document.getElementById("image5").src = "http://img05.deviantart.net/5c20/i/2007/210/3/6/zoidberg_vector_by_enohp.jpg"
+    }
+}
+function run6(){
+    if(document.getElementById("image6").src == "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/82/8296488abd38512c5a116a3caf15ef83a1098777_full.jpg") {
+        document.getElementById("image6").src = "http://images.akamai.steamusercontent.com/ugc/474299521462795253/0A6DDA97F9EC6233F80645F7963C349800D42B4E/"
+    }
+    else {
+        document.getElementById("image6").src = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/82/8296488abd38512c5a116a3caf15ef83a1098777_full.jpg"
+    }
+}
+function run7(){
+    if(document.getElementById("image7").src == "http://netdna.walyou.netdna-cdn.com/wp-content/uploads//2012/04/Zoidberg-Jesus-e1335468273542.jpg") {
+        document.getElementById("image7").src = "https://s-media-cache-ak0.pinimg.com/236x/c2/1f/07/c21f070260f2fb7f40cd0d14726d63f9.jpg"
+    }
+    else {
+        document.getElementById("image7").src = "http://netdna.walyou.netdna-cdn.com/wp-content/uploads//2012/04/Zoidberg-Jesus-e1335468273542.jpg"
+    }
+}
+function run8(){
+    if(document.getElementById("image8").src == "http://i2.wp.com/images.geeknative.com.s3.amazonaws.com/wp-content/uploads/2013/05/Zoidberg-bottom-reference.jpg") {
+        document.getElementById("image8").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQijW4dMpNYGVU7HzhYdFK8xV0x-Lo9I59cEsYSPtk4u23Srp_4"
+    }
+    else {
+        document.getElementById("image8").src = "http://i2.wp.com/images.geeknative.com.s3.amazonaws.com/wp-content/uploads/2013/05/Zoidberg-bottom-reference.jpg"
+    }
+}
+function run9(){
+    if(document.getElementById("image9").src == "http://files.tested.com/photos/2013/10/24/55-54660-jared-1382633827.jpg") {
+        document.getElementById("image9").src = "http://0.media.dorkly.cvcdn.com/18/90/4bbee580c3a15c4b6ec967738f97dc5d.jpg"
+    }
+    else {
+        document.getElementById("image9").src = "http://files.tested.com/photos/2013/10/24/55-54660-jared-1382633827.jpg"
+    }
+}
